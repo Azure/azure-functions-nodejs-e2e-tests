2d64586168ca696d40cb0091faed1d22ae72a4c3
Eric Jizba <erijiz@microsoft.com>
2023-04-28 17:25:09 -0700

    Cookies test

---
+++ app/v4/src/functions/httpCookies.ts
@@ -0,0 +1,45 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License.
+
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
+
+export async function httpCookies(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    return {
+        cookies: [
+            {
+                name: 'mycookie',
+                value: 'myvalue',
+                maxAge: 200000,
+            },
+            {
+                name: 'mycookie2',
+                value: 'myvalue',
+                path: '/',
+                maxAge: <any>'200000',
+            },
+            {
+                name: 'mycookie3-expires',
+                value: 'myvalue3-expires',
+                maxAge: 0,
+            },
+            {
+                name: 'mycookie4-samesite-lax',
+                value: 'myvalue',
+                sameSite: 'Lax',
+            },
+            {
+                name: 'mycookie5-samesite-strict',
+                value: 'myvalue',
+                sameSite: 'Strict',
+            },
+        ],
+    };
+}
+
+app.http('httpCookies', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: httpCookies,
+});
--- src/http.test.ts
+++ src/http.test.ts
@@ -52,6 +52,17 @@ describe('http', () => {
         expect(response.status).to.equal(404);
     });
 
+    it('Cookies', async () => {
+        const response = await fetch(getFuncUrl('httpCookies'));
+        const body = await response.text();
+        expect(body).to.equal('');
+        expect(response.status).to.equal(200);
+        const cookies = response.headers.get('Set-Cookie');
+        expect(cookies).to.equal(
+            'mycookie=myvalue; max-age=200000; path=/, mycookie2=myvalue; max-age=200000; path=/, mycookie3-expires=myvalue3-expires; max-age=0; path=/, mycookie4-samesite-lax=myvalue; path=/; samesite=lax, mycookie5-samesite-strict=myvalue; path=/; samesite=strict'
+        );
+    });
+
     describe('v3 only', () => {
         before(function (this: Mocha.Context) {
             if (model !== 'v3') {
