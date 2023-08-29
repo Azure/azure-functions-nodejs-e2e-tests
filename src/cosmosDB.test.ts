754c6307792e00e1beb76705a0222bfaa71eac62
Eric Jizba <erijiz@microsoft.com>
2023-08-29 13:20:08 -0700

    Add test for Cosmos DB input binding (#7)

--- src/cosmosDB.test.ts
+++ src/cosmosDB.test.ts
@@ -2,21 +2,29 @@
 // Licensed under the MIT License.
 
 import { CosmosClient } from '@azure/cosmos';
+import { expect } from 'chai';
+import { default as fetch } from 'node-fetch';
+import { getFuncUrl } from './constants';
 import { waitForOutput } from './global.test';
 import { cosmosDBConnectionString } from './resources/connectionStrings';
 import { container1Name, dbName } from './resources/cosmosDB';
 import { getRandomTestData } from './utils/getRandomTestData';
 
 describe('cosmosDB', () => {
-    it('trigger and output', async () => {
+    it('trigger, output, input', async () => {
         const client = new CosmosClient(cosmosDBConnectionString);
         const container = client.database(dbName).container(container1Name);
         const message = getRandomTestData();
-        await container.items.create({ message });
+        const createdItem = await container.items.create({ message, _partitionKey: 'testPartKey' });
 
         await waitForOutput(`cosmosDBTrigger1 processed 1 documents`);
         await waitForOutput(`cosmosDBTrigger1 was triggered by "${message}"`);
         await waitForOutput(`cosmosDBTrigger2 processed 1 documents`);
         await waitForOutput(`cosmosDBTrigger2 was triggered by "${message}"`);
+
+        const url = `${getFuncUrl('cosmosDBInput1')}?id=${createdItem.item.id}`;
+        const response = await fetch(url);
+        const body = await response.text();
+        expect(body).to.equal(message);
     });
 });
