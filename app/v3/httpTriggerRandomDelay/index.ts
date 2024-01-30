c6e3b10cda65b9142f3c531ef61d76fc2ed10bde
Eric Jizba <erijiz@microsoft.com>
2024-01-29 17:21:04 -0800

    Http stream end to end tests

---
+++ app/v3/httpTriggerRandomDelay/index.ts
@@ -0,0 +1,19 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License.
+
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { addRandomDelay } from '../utils/getRandomTestData';
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log(`Http function processed request for url "${req.url}"`);
+
+    await addRandomDelay();
+
+    const name = req.query.name || req.body || 'world';
+
+    context.res = {
+        body: `Hello, ${name}!`,
+    };
+};
+
+export default httpTrigger;
