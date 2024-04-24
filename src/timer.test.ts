7f2336d407dbc122c3632cd15fd179cf429998ad
Eric Jizba <erijiz@microsoft.com>
2024-04-23 17:27:31 -0700

    Add test for retry policy

--- src/timer.test.ts
+++ src/timer.test.ts
@@ -7,4 +7,10 @@ describe('timer', () => {
     it('hello world', async () => {
         await waitForOutput('timerTrigger was triggered', { checkFullOutput: true });
     });
+
+    it('retry', async () => {
+        await waitForOutput('timerTriggerWithRetry error on attempt 0', { checkFullOutput: true });
+        await waitForOutput('timerTriggerWithRetry error on attempt 1', { checkFullOutput: true });
+        await waitForOutput('timerTriggerWithRetry pass on attempt 2', { checkFullOutput: true });
+    });
 });
