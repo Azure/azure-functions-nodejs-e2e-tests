ae1eea31412d775c4b6b19bf022bff2c0c9aa92b
Evan Roman <evanroman@microsoft.com>
2025-07-17 10:02:32 -0500

    add http

--- app/v3/helloWorld/index.ts
+++ app/v3/helloWorld/index.ts
@@ -1,16 +1,16 @@
-// // Copyright (c) .NET Foundation. All rights reserved.
-// // Licensed under the MIT License.
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License.
 
-// import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
-// const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-//     context.log(`Http function processed request for url "${req.url}"`);
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log(`Http function processed request for url "${req.url}"`);
 
-//     const name = req.query.name || req.body || 'world';
+    const name = req.query.name || req.body || 'world';
 
-//     context.res = {
-//         body: `Hello, ${name}!`,
-//     };
-// };
+    context.res = {
+        body: `Hello, ${name}!`,
+    };
+};
 
-// export default httpTrigger;
+export default httpTrigger;
