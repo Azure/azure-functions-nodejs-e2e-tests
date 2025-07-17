2640c8ee02bc38bf57197785b4cad91ca7edfae6
Evan Roman <evanroman@microsoft.com>
2025-07-17 12:28:18 -0500

    test

--- app/v4/src/functions/storageBlobTrigger.ts
+++ app/v4/src/functions/storageBlobTrigger.ts
@@ -1,15 +1,15 @@
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT License.
+// // Copyright (c) .NET Foundation. All rights reserved.
+// // Licensed under the MIT License.
 
-import { app, InvocationContext } from '@azure/functions';
+// import { app, InvocationContext } from '@azure/functions';
 
-export async function storageBlobTrigger(blob: Buffer, context: InvocationContext): Promise<void> {
-    const blobPath = context.triggerMetadata.blobTrigger;
-    context.log(`storageBlobTrigger was triggered by blob "${blobPath}" with content "${blob.toString()}"`);
-}
+// export async function storageBlobTrigger(blob: Buffer, context: InvocationContext): Promise<void> {
+//     const blobPath = context.triggerMetadata.blobTrigger;
+//     context.log(`storageBlobTrigger was triggered by blob "${blobPath}" with content "${blob.toString()}"`);
+// }
 
-app.storageBlob('storageBlobTrigger', {
-    path: 'e2e-test-container/e2e-test-blob-trigger',
-    connection: 'AzureWebJobsStorage',
-    handler: storageBlobTrigger,
-});
+// app.storageBlob('storageBlobTrigger', {
+//     path: 'e2e-test-container/e2e-test-blob-trigger',
+//     connection: 'AzureWebJobsStorage',
+//     handler: storageBlobTrigger,
+// });
