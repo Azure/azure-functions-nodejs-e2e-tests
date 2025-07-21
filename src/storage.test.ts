655abcd3bfe7d5cb54cfba2505e68c7e038ce2f1
Evan Roman <evanroman@microsoft.com>
2025-07-21 15:30:02 -0500

    test

--- src/storage.test.ts
+++ src/storage.test.ts
@@ -2,44 +2,44 @@
 // Licensed under the MIT License.
 
 import { ContainerClient } from '@azure/storage-blob';
-// import { QueueClient } from '@azure/storage-queue';
+import { QueueClient } from '@azure/storage-queue';
 // import { expect } from 'chai';
-// import { default as fetch } from 'node-fetch';
-// import { getFuncUrl } from './constants';
+import { default as fetch } from 'node-fetch';
+import { getFuncUrl } from './constants';
 import { model, waitForOutput } from './global.test';
 import { getRandomTestData } from './utils/getRandomTestData';
 import { storageConnectionString } from './utils/connectionStrings';
 
 describe('storage', () => {
-//     it('queue trigger and output', async () => {
-//         const client = new QueueClient(storageConnectionString, 'e2e-test-queue-trigger-and-output');
-//         await client.createIfNotExists();
+    it('queue trigger and output', async () => {
+        const client = new QueueClient(storageConnectionString, 'e2e-test-queue-trigger-and-output');
+        await client.createIfNotExists();
 
-//         const message = getRandomTestData();
-//         await client.sendMessage(Buffer.from(message).toString('base64'));
+        const message = getRandomTestData();
+        await client.sendMessage(Buffer.from(message).toString('base64'));
 
-//         await waitForOutput(`storageQueueTriggerAndOutput was triggered by "${message}"`);
-//         await waitForOutput(`storageQueueTrigger was triggered by "${message}"`);
-//     });
+        await waitForOutput(`storageQueueTriggerAndOutput was triggered by "${message}"`);
+        await waitForOutput(`storageQueueTrigger was triggered by "${message}"`);
+    });
 
-    // it('queue extra output', async () => {
-    //     const url = getFuncUrl('httpTriggerStorageQueueOutput');
+    it('queue extra output', async () => {
+        const url = getFuncUrl('httpTriggerStorageQueueOutput');
 
-    //     // single
-    //     const message = getRandomTestData();
-    //     await fetch(url, { method: 'POST', body: JSON.stringify({ output: message }) });
-    //     await waitForOutput(`storageQueueTrigger was triggered by "${message}"`);
+        // single
+        const message = getRandomTestData();
+        await fetch(url, { method: 'POST', body: JSON.stringify({ output: message }) });
+        await waitForOutput(`storageQueueTrigger was triggered by "${message}"`);
 
-    //     // bulk
-    //     const bulkMsgs: string[] = [];
-    //     for (let i = 0; i < 5; i++) {
-    //         bulkMsgs.push(getRandomTestData());
-    //     }
-    //     await fetch(url, { method: 'POST', body: JSON.stringify({ output: bulkMsgs }) });
-    //     for (const msg of bulkMsgs) {
-    //         await waitForOutput(`storageQueueTrigger was triggered by "${msg}"`);
-    //     }
-    // });
+        // bulk
+        const bulkMsgs: string[] = [];
+        for (let i = 0; i < 5; i++) {
+            bulkMsgs.push(getRandomTestData());
+        }
+        await fetch(url, { method: 'POST', body: JSON.stringify({ output: bulkMsgs }) });
+        for (const msg of bulkMsgs) {
+            await waitForOutput(`storageQueueTrigger was triggered by "${msg}"`);
+        }
+    });
 
     it('blob trigger and output', async () => {
         const containerName = 'e2e-test-container';
