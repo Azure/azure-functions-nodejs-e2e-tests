2a92719aa872f389b9853307a47a4b33ae32d7d4
Evan Roman <evanroman@microsoft.com>
2025-07-22 11:55:57 -0500

    add

--- app/v4-oldConfig/src/functions/httpTriggerCosmosDBInput.ts
+++ app/v4-oldConfig/src/functions/httpTriggerCosmosDBInput.ts
@@ -1,27 +1,27 @@
-// // Copyright (c) .NET Foundation. All rights reserved.
-// // Licensed under the MIT License.
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License.
 
-// import { app, HttpRequest, HttpResponseInit, input, InvocationContext } from '@azure/functions';
+import { app, HttpRequest, HttpResponseInit, input, InvocationContext } from '@azure/functions';
 
-// const cosmosInput = input.cosmosDB({
-//     databaseName: 'e2eTestDB',
-//     collectionName: 'e2eTestContainerTriggerAndOutput',
-//     id: '{Query.id}',
-//     partitionKey: 'testPartKey',
-//     connectionStringSetting: 'CosmosDBConnection',
-// });
+const cosmosInput = input.cosmosDB({
+    databaseName: 'e2eTestDB',
+    collectionName: 'e2eTestContainerTriggerAndOutput',
+    id: '{Query.id}',
+    partitionKey: 'testPartKey',
+    connectionStringSetting: 'CosmosDBConnection',
+});
 
-// export async function httpTriggerCosmosDBInput(
-//     _request: HttpRequest,
-//     context: InvocationContext
-// ): Promise<HttpResponseInit> {
-//     const doc = context.extraInputs.get(cosmosInput);
-//     return { body: (<any>doc).testData };
-// }
+export async function httpTriggerCosmosDBInput(
+    _request: HttpRequest,
+    context: InvocationContext
+): Promise<HttpResponseInit> {
+    const doc = context.extraInputs.get(cosmosInput);
+    return { body: (<any>doc).testData };
+}
 
-// app.http('httpTriggerCosmosDBInput', {
-//     methods: ['GET', 'POST'],
-//     authLevel: 'anonymous',
-//     extraInputs: [cosmosInput],
-//     handler: httpTriggerCosmosDBInput,
-// });
+app.http('httpTriggerCosmosDBInput', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    extraInputs: [cosmosInput],
+    handler: httpTriggerCosmosDBInput,
+});
