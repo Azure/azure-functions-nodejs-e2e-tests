40662b7bf23fa1d82df5ba12e694120f58881bad
Evan Roman <evanroman@microsoft.com>
2025-07-22 14:33:20 -0500

    adjust

--- app/v3/httpTriggerTableInput/index.ts
+++ app/v3/httpTriggerTableInput/index.ts
@@ -1,13 +1,13 @@
-// // Copyright (c) .NET Foundation. All rights reserved.
-// // Licensed under the MIT License.
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License.
 
-// import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
-// const httpTriggerTableInput: AzureFunction = async function (context: Context, request: HttpRequest): Promise<void> {
-//     context.log(`httpTriggerTableInput was triggered`);
-//     context.res = {
-//         body: context.bindings.inputItem,
-//     };
-// };
+const httpTriggerTableInput: AzureFunction = async function (context: Context, request: HttpRequest): Promise<void> {
+    context.log(`httpTriggerTableInput was triggered`);
+    context.res = {
+        body: context.bindings.inputItem,
+    };
+};
 
-// export default httpTriggerTableInput;
+export default httpTriggerTableInput;
