3f6b4b0f86506c6e652aa55fab4b2f35bd6b612f
Evan Roman <evanroman@microsoft.com>
2025-07-29 13:32:17 -0500

    adjust

--- src/utils/connectionStrings.ts
+++ src/utils/connectionStrings.ts
@@ -9,5 +9,6 @@ export let serviceBusConnectionString: string;
 export async function initializeConnectionStrings(): Promise<void> {
     storageConnectionString = "UseDevelopmentStorage=true";
     cosmosDBConnectionString = "AccountEndpoint=http://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
-    serviceBusConnectionString = eventHubConnectionString = "Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
+    eventHubConnectionString = "Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
+    serviceBusConnectionString = "Endpoint=sb://localhost:5673;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
 }
