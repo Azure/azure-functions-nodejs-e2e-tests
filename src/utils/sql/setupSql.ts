ff48ddfe00810bc218949dc7d9cfe881cf159f00
Evan Roman <evanroman@microsoft.com>
2025-08-01 16:56:16 -0500

    fix

--- src/utils/sql/setupSql.ts
+++ src/utils/sql/setupSql.ts
@@ -21,8 +21,20 @@ export const sqlSecretName = 'e2eSqlSecret';
  * create tables and enable change tracking for trigger
  */
 export async function runSqlSetupQueries() {
-    const connectionString = await getSqlConnectionString();
-    const poolConnection = await createPoolConnnection(connectionString);
+    // const connectionString = await getSqlConnectionString();
+    const config: sql.config = {
+        user: 'sa',
+        password: 'placeholderForContainer@1475',
+        server: 'localhost', // or 'sqlserver' if running from another container
+        database: dbName, // or create your own
+        port: 1433,
+        options: {
+            encrypt: false, // set to true if using Azure or SSL
+            trustServerCertificate: true,
+        }
+    };
+    const poolConnection = await sql.connect(config)
+    // const poolConnection = await createPoolConnnection();
 
     try {
         await poolConnection
@@ -42,37 +54,37 @@ export async function runSqlSetupQueries() {
     }
 }
 
-export async function createPoolConnnection(connectionString: string): Promise<sql.ConnectionPool> {
-    const retries = 5;
-    return retry(
-        async (currentAttempt: number) => {
-            if (currentAttempt > 1) {
-                console.log(
-                    `${new Date().toISOString()}: Retrying sql connect. Attempt ${currentAttempt}/${retries + 1}`
-                );
-            }
-            return sql.connect(connectionString);
-        },
-        {
-            retries: retries,
-            minTimeout: 5 * 1000,
-            onFailedAttempt: (error) => {
-                if (!/ip address/i.test(error?.message || '')) {
-                    throw error; // abort for an unrecognized error
-                } else if (error.retriesLeft > 0) {
-                    console.log(`Warning: Failed to sql connect with error "${error.message}"`);
-                }
-            },
-        }
-    );
-}
+// export async function createPoolConnnection(connectionString: string): Promise<sql.ConnectionPool> {
+//     const retries = 5;
+//     return retry(
+//         async (currentAttempt: number) => {
+//             if (currentAttempt > 1) {
+//                 console.log(
+//                     `${new Date().toISOString()}: Retrying sql connect. Attempt ${currentAttempt}/${retries + 1}`
+//                 );
+//             }
+//             return sql.connect(connectionString);
+//         },
+//         {
+//             retries: retries,
+//             minTimeout: 5 * 1000,
+//             onFailedAttempt: (error) => {
+//                 if (!/ip address/i.test(error?.message || '')) {
+//                     throw error; // abort for an unrecognized error
+//                 } else if (error.retriesLeft > 0) {
+//                     console.log(`Warning: Failed to sql connect with error "${error.message}"`);
+//                 }
+//             },
+//         }
+//     );
+// }
 
-export async function getSqlConnectionString(): Promise<string> {
-    const server = 'localhost'; // or 'sqlserver' if running in another container
-    const port = 1433;
-    const user = 'sa';
-    const password = 'thisPasswordDoesntMatter@1475'; // use your actual password
-    const database = dbName;
+// export async function getSqlConnectionString(): Promise<string> {
+//     const server = 'localhost'; // or 'sqlserver' if running in another container
+//     const port = 1433;
+//     const user = 'sa';
+//     const password = 'thisPasswordDoesntMatter@1475';
+//     const database = dbName;
 
-    return `Server=${server},${port};Database=${database};User Id=${user};Password=${password};Encrypt=false;TrustServerCertificate=true;`;
-}
+//     return `Server=${server},${port};Database=${database};User Id=${user};Password=${password};Encrypt=false;TrustServerCertificate=true;`;
+// }
