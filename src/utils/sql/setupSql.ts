b919ad8d5fa0d283dd34d9708de6852f937551da
Evan Roman <evanroman@microsoft.com>
2025-08-06 17:13:54 -0500

    fix

--- src/utils/sql/setupSql.ts
+++ src/utils/sql/setupSql.ts
@@ -6,66 +6,26 @@ import * as sql from 'mssql';
 import { sqlConnectionString } from '../connectionStrings';
 import { Sql } from '../../constants';
 
-export async function createPoolConnnection(): Promise<sql.ConnectionPool> {
-    // const password = process.env.SA_PASSWORD;
-    // if (!password || password.trim() === '') {
-    //     throw new Error('Missing required environment variable: SA_PASSWORD');
-    // }
-
-    // const config = {
-    //     user: 'sa',
-    //     password: password,
-    //     server: 'localhost',
-    //     port: 15433,
-    //     database: 'master',
-    //     options: {
-    //         encrypt: false, // set to true if connecting to Azure SQL
-    //         trustServerCertificate: true,
-    //     }
-    // };
-
-    if (!sqlConnectionString || sqlConnectionString.trim() === '') {
-        throw new Error('Missing required environment variable: SqlConnection');
-    }
-    
+export async function runSqlSetupQueries() {
     const poolConnection = await sql.connect(sqlConnectionString);
-    await poolConnection
+    try {
+        await poolConnection
             .request()
             .query(`ALTER DATABASE ${Sql.dbName} SET CHANGE_TRACKING = ON (CHANGE_RETENTION = 2 DAYS, AUTO_CLEANUP = ON);`);
 
-    for (const table of [Sql.sqlTriggerTable, Sql.sqlNonTriggerTable]) {
-        await poolConnection
-            .request()
-            .query(
-                `CREATE TABLE dbo.${table} ([id] UNIQUEIDENTIFIER PRIMARY KEY, [testData] NVARCHAR(200) NOT NULL);`
-            );
-        await poolConnection.request().query(`ALTER TABLE dbo.${table} ENABLE CHANGE_TRACKING;`);
+        for (const table of [Sql.sqlTriggerTable, Sql.sqlNonTriggerTable]) {
+            await poolConnection
+                .request()
+                .query(
+                    `CREATE TABLE dbo.${table} ([id] UNIQUEIDENTIFIER PRIMARY KEY, [testData] NVARCHAR(200) NOT NULL);`
+                );
+            await poolConnection.request().query(`ALTER TABLE dbo.${table} ENABLE CHANGE_TRACKING;`);
+        }
+    } finally {
+        await poolConnection.close();
     }
-
-    return poolConnection;
 }
 
-// export async function createPoolConnnection(connectionString: string): Promise<sql.ConnectionPool> {
-//     const retries = 5;
-//     return retry(
-//         async (currentAttempt: number) => {
-//             if (currentAttempt > 1) {
-//                 console.log(
-//                     `${new Date().toISOString()}: Retrying sql connect. Attempt ${currentAttempt}/${retries + 1}`
-//                 );
-//             }
-//             return sql.connect(connectionString);
-//         },
-//         {
-//             retries: retries,
-//             minTimeout: 5 * 1000,
-//             onFailedAttempt: (error) => {
-//                 if (!/ip address/i.test(error?.message || '')) {
-//                     throw error; // abort for an unrecognized error
-//                 } else if (error.retriesLeft > 0) {
-//                     console.log(`Warning: Failed to sql connect with error "${error.message}"`);
-//                 }
-//             },
-//         }
-//     );
-// }
+export async function createPoolConnnection() {
+    return await sql.connect(sqlConnectionString);
+}
