0f5e30c7b3edb213d580f020cdf13f8fb8cff372
Evan Roman <evanroman@microsoft.com>
2025-08-07 02:55:05 -0500

    fix

--- src/utils/connectionStrings.ts
+++ src/utils/connectionStrings.ts
@@ -9,14 +9,9 @@ export let sqlConnectionString: string;
 export let sqlTestConnectionString: string;
 
 export async function initializeConnectionStrings(): Promise<void> {
-    const password = process.env.SA_PASSWORD;
-    if (!password || password.trim() === '') {
-        throw new Error('Missing required environment variable: SA_PASSWORD');
-    }
-
     storageConnectionString = "UseDevelopmentStorage=true";
     cosmosDBConnectionString = "AccountEndpoint=http://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
     serviceBusConnectionString = eventHubConnectionString = "Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
-    sqlConnectionString = `Server=localhost,15433;Database=master;User Id=sa;Password=${password};Encrypt=false;TrustServerCertificate=true;`;
-    sqlTestConnectionString = `Server=localhost,15433;Database=e2eTestDB;User Id=sa;Password=${password};Encrypt=false;TrustServerCertificate=true;`;
+    sqlConnectionString = `Server=localhost,15433;Database=master;User Id=sa;Password=${process.env.SA_PASSWORD};Encrypt=false;TrustServerCertificate=true;`;
+    sqlTestConnectionString = `Server=localhost,15433;Database=e2eTestDB;User Id=sa;Password=${process.env.SA_PASSWORD};Encrypt=false;TrustServerCertificate=true;`;
 }
