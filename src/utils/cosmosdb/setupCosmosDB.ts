4c41e9df873773d2956f9855ae4a201471300bbf
Evan Roman <evanroman@microsoft.com>
2025-08-08 01:00:23 -0500

    fix

--- src/utils/cosmosdb/setupCosmosDB.ts
+++ src/utils/cosmosdb/setupCosmosDB.ts
@@ -5,21 +5,30 @@
 // Otherwise the following error will occur:
 // Microsoft.Azure.Cosmos.Client: This builder instance has already been used to build a processor. Create a new instance to build another.
 
-// import { CosmosClient, PartitionKeyKind } from '@azure/cosmos';
-// import { cosmosDBConnectionString } from '../connectionStrings';
-// import { CosmosDB } from '../../constants';
+import { CosmosClient, PartitionKeyKind } from '@azure/cosmos';
+import { cosmosDBConnectionString } from '../connectionStrings';
+import { CosmosDB } from '../../constants';
+import { delay } from '../delay';
 
 export async function setupCosmosDB() {
-  throw new Error('When do we throw?');
-  // const partitionKeyPath = `/${CosmosDB.partitionKey}`;
-  // const client = new CosmosClient(cosmosDBConnectionString);
-  // await client.databases.createIfNotExists({ id: CosmosDB.triggerDatabaseName });
-  // await client.database(CosmosDB.triggerDatabaseName).containers.createIfNotExists({
-  //   id: CosmosDB.triggerContainerName,
-  //   partitionKey: { paths: [partitionKeyPath], kind: PartitionKeyKind.Hash }
-  // });
-  // await client.database(CosmosDB.triggerDatabaseName).containers.createIfNotExists({
-  //   id: CosmosDB.triggerAndOutputContainerName,
-  //   partitionKey: { paths: [partitionKeyPath], kind: PartitionKeyKind.Hash }
-  // });
+  try {
+    const partitionKeyPath = `/${CosmosDB.partitionKey}`;
+    if (!cosmosDBConnectionString) {
+      throw new Error('CosmosDB connection string is not set');
+    }
+    const client = new CosmosClient(cosmosDBConnectionString);
+    await client.databases.createIfNotExists({ id: CosmosDB.triggerDatabaseName });
+    await client.database(CosmosDB.triggerDatabaseName).containers.createIfNotExists({
+      id: CosmosDB.triggerContainerName,
+      partitionKey: { paths: [partitionKeyPath], kind: PartitionKeyKind.Hash }
+    });
+    await client.database(CosmosDB.triggerDatabaseName).containers.createIfNotExists({
+      id: CosmosDB.triggerAndOutputContainerName,
+      partitionKey: { paths: [partitionKeyPath], kind: PartitionKeyKind.Hash }
+    });
+} catch (error) {
+    console.error('Error setting up CosmosDB:', error);
+    throw error;
+  }
+  await delay(30000);
 }
